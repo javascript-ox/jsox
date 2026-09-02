67cad2212b3fa9651f5c8eadfe6fe3571a0a14e5
James Landrum <me@jameslandrum.com>
2026-09-02 10:50:00 -0400

    feat(examples): add four library integrations

--- apps/pages/src/routes.js
+++ apps/pages/src/routes.js
@@ -4,11 +4,15 @@ export const routes = new Map([
   ["/tutorial", "jsox-tutorial-page"],
   ["/hello", "jsox-hello-page"],
   ["/counter", "jsox-counter-page"],
+  ["/d3", "jsox-d3-page"],
   ["/effect", "jsox-effect-page"],
+  ["/gsap", "jsox-gsap-page"],
   ["/todo", "jsox-todo-page"],
   ["/pojo", "jsox-pojo-page"],
   ["/rxjs", "jsox-rxjs-page"],
   ["/web-components", "jsox-web-components-page"],
+  ["/xstate", "jsox-xstate-page"],
+  ["/zod", "jsox-zod-page"],
 ]);
 
 export function currentPath(hash = location.hash) {
---
+++ examples/gsap/vite.config.js
@@ -0,0 +1,4 @@
+import { defineConfig } from "vite";
+import jsox from "@js-ox/compiler/vite";
+
+export default defineConfig({ plugins: [jsox()], base: "./", server: { port: 5183, strictPort: true } });
