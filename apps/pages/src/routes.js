b5f65f283cb8081e4861b2f7a980dc7415cebdee
James Landrum <me@jameslandrum.com>
2026-09-02 11:10:50 -0400

    feat(examples): add hypermedia and 3D integrations

--- apps/pages/src/routes.js
+++ apps/pages/src/routes.js
@@ -1,19 +1,24 @@
 export const routes = new Map([
-  ["/", "jsox-home-page"],
-  ["/start", "jsox-start-page"],
-  ["/tutorial", "jsox-tutorial-page"],
-  ["/hello", "jsox-hello-page"],
-  ["/counter", "jsox-counter-page"],
-  ["/d3", "jsox-d3-page"],
-  ["/effect", "jsox-effect-page"],
-  ["/gsap", "jsox-gsap-page"],
-  ["/todo", "jsox-todo-page"],
-  ["/pojo", "jsox-pojo-page"],
-  ["/rxjs", "jsox-rxjs-page"],
-  ["/web-components", "jsox-web-components-page"],
-  ["/xstate", "jsox-xstate-page"],
-  ["/zod", "jsox-zod-page"],
+  ["/", () => import("./pages/home.jsox").then(({ Home }) => Home())],
+  ["/start", () => import("./pages/docs.jsox").then(({ QuickStart }) => QuickStart())],
+  ["/tutorial", () => import("./pages/docs.jsox").then(({ Tutorial }) => Tutorial())],
+  ["/hello", () => import("@js-ox/hello").then(({ Hello }) => Hello())],
+  ["/counter", () => import("@js-ox/counter").then(({ Counter }) => Counter())],
+  ["/d3", () => import("@js-ox/d3").then(({ D3Example }) => D3Example())],
+  ["/effect", () => import("@js-ox/effect").then(({ EffectExample }) => EffectExample())],
+  ["/gsap", () => import("@js-ox/gsap").then(({ GsapExample }) => GsapExample())],
+  ["/htmx", () => import("@js-ox/htmx").then(({ HtmxExample }) => HtmxExample())],
+  ["/todo", () => import("@js-ox/todo").then(({ Todo }) => Todo())],
+  ["/pojo", () => import("@js-ox/pojo").then(({ Pojo }) => Pojo())],
+  ["/rxjs", () => import("@js-ox/rxjs").then(({ Rxjs }) => Rxjs())],
+  ["/stimulus", () => import("@js-ox/stimulus").then(({ StimulusExample }) => StimulusExample())],
+  ["/three", () => import("@js-ox/three").then(({ ThreeExample }) => ThreeExample())],
+  ["/web-components", () => import("@js-ox/web-components-example").then(({ WebComponents }) => WebComponents())],
+  ["/xstate", () => import("@js-ox/xstate").then(({ XStateExample }) => XStateExample())],
+  ["/zod", () => import("@js-ox/zod").then(({ ZodExample }) => ZodExample())],
 ]);
+
+const notFound = () => import("./pages/not-found.jsox").then(({ NotFound }) => NotFound());
 
 export function currentPath(hash = location.hash) {
   const path = hash.replace(/^#/, "") || "/";
@@ -21,5 +26,5 @@ export function currentPath(hash = location.hash) {
 }
 
 export function resolveRoute(path) {
-  return routes.get(path) ?? "jsox-missing-page";
+  return routes.get(path) ?? notFound;
 }
